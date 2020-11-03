32be5b971bce03cf52031adeabb45068a0361384
odwanodada <odwanodada22@gmail.com>
2020-11-03 15:57:57 +0200

    Js Variable

---
+++ VariablesExercise.js
@@ -0,0 +1,13 @@
+let name = "Odwa";
+let surname = "Nodada";
+console.log(name + surname);
+document.write(name + surname)
+document.write("<br/><br/>");
+
+let numVar = 5;
+console.log(numVar*5);
+document.write(numVar);
+document.write("<br/><br/>");
+
+const Odwa = "Nodada";
+console.log(Odwa);
