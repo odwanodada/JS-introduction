c7c7d983e5c76897ee9b4df9ecbb4a85323608c9
odwanodada <odwanodada22@gmail.com>
2020-11-03 15:34:07 +0200

    JavascriptIntro

---
+++ intro.js
@@ -0,0 +1,53 @@
+//document.write("<br/><br/><br/><br/>");
+//document.write(5+6);
+//let name = "Odwa";
+//console.log("My name is", name);
+//document.write("<br/><br/><br/>");
+
+
+//let myname = "Sive";
+//console.log(myname);
+//document.write("<br/><br/><br/><br/>");
+
+
+//let firstname4 = "Class1";
+//console.log(firstname4)
+//document.write("<br/><br/><br/><br/>");
+
+//let num1 = 5;
+//document.write(num1);
+
+let x=5, y=9, z=3;
+document.write(z)
+document.write("<br/><br/><br/><br/>");
+
+//let a=b=5;
+//document(a)
+
+const pi=3.142;
+document.write(pi)
+document.write("<br/><br/><br/><br/>");
+
+const car = "Toyota";
+document.write(car); 
+document.write("<br/><br/><br/><br/>");
+
+const person = {name:"Nodata",Age:21 ,id:909889989090};
+let {name,Age,id} = person;
+document.write(name);
+document.write("<br/><br/>");
+document.write(Age);
+document.write("<br/><br/>");
+document.write(id);
+document.write("<br/><br/><br/><br/>");
+
+const bus = {bus_name:"CitytoCity",color:"blue",ticket:500};
+let {bus_name,color,ticket} = bus;
+document.write(bus_name + "<br/><br/>");
+document.write(color + "<br/><br/>");
+document.write(ticket + "<br/><br/>");
+document.write("<br/><br/>");
+
+let a,b,c;a=b=c=0;
+c++;
+document.write(c);
