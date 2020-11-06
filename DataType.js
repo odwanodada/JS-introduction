d6e3f029414b6a414c670260c76731e72b8e0d28
odwanodada <odwanodada22@gmail.com>
2020-11-06 14:16:21 +0200

    DATATypes

---
+++ DataType.js
@@ -0,0 +1,48 @@
+let myname = "Steve";
+document.write(typeof(myname) + "<br/><br/>");//typeof is use to check the datatype
+
+
+let mysentence = 'Tamsyn said: "We are all crazy"';//it will display everything
+document.write(mysentence + "<br/><br/>");
+document.write(typeof(mysentence) + "<br/><br/>");
+
+let str = "Hello There";
+let str2 = 'Single quotes works fine';
+let phrase = 'can embed S{str}';
+
+document.write(str + "<br/><br/>");
+document.write(str2 + "<br/><br/>");
+document.write(phrase + "<br/><br/>");
+
+let age = 20;
+document.write(typeof(age) + "<br/><br/>");
+
+let my_partner_age = 34.7;
+document.write(typeof(my_partner_age) + "<br/><br/>");
+
+let my_name;
+document.write(typeof(my_name) + "<br/><br/>");
+
+let num3 = Infinity;
+document.write(typeof(num3) + "<br/><br/>");
+
+let num4 = 'something here'/2;
+document.write(typeof(num4) + "<br/><br/>");
+
+document.write(3>5 + "<br/><br/>");
+let is_greater_or_less = 3>5;
+document.write(typeof(is_greater_or_less) + "<br/><br/>");
+
+let x = "Sive";
+let result = x+2+2;
+document.write(result + "<br/><br/>");
+
+let lastName = null;
+document.write(typeof(lastName)+ "<br/><br/>");
+
+
+
+
+
+
+
